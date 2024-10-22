fa528b73970b941793d34f8ce2bca3b20d83671b
Joseph Wambua <josefwambua2@gmail.com>
2024-10-22 15:11:36 +0300

    disabling-cart

--- products/detail-product.php
+++ products/detail-product.php
@@ -1,54 +1,72 @@
-<?php require '../includes/header.php' ?>
-<?php require '../config/config.php' ?>
+<?php require '../includes/header.php'; ?>
+<?php require '../config/config.php'; ?>
 
 <?php
-if(isset($_GET['id'])){
-    $id = $_GET['id'];
-    $select = $conn->query("SELECT * FROM products WHERE status  = 1 AND id=$id");
-    $select->execute();
 
+// Ensure the database connection throws exceptions
+$conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+// Handle the POST request for adding to cart
+if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['submit'])) {
+    $pro_id = $_POST['pro_id'];
+    $pro_title = $_POST['pro_title'];
+    $pro_image = $_POST['pro_image'];
+    $pro_price = $_POST['pro_price'];
+    $pro_qty = $_POST['pro_qty'];
+    $user_id = $_POST['user_id'];
+
+    // Check if all required fields are filled
+    if (!empty($pro_id) && !empty($pro_title) && !empty($pro_image) && !empty($pro_price) && !empty($pro_qty) && !empty($user_id)) {
+        $insert = $conn->prepare("INSERT INTO cart (pro_id, pro_title, pro_image, pro_price, pro_qty, user_id) 
+        VALUES (:pro_id, :pro_title, :pro_image, :pro_price, :pro_qty, :user_id)");
+
+        try {
+            $insert->execute([
+                ':pro_id' => $pro_id,
+                ':pro_title' => $pro_title,
+                ':pro_image' => $pro_image,
+                ':pro_price' => $pro_price,
+                ':pro_qty' => $pro_qty,
+                ':user_id' => $user_id,
+            ]);
+            // Fetch the last inserted ID if needed
+            $lastInsertId = $conn->lastInsertId();
+            echo json_encode(['status' => 'success', 'message' => 'Product added successfully']);
+            exit;
+        } catch (PDOException $e) {
+            echo json_encode(['status' => 'error', 'message' => 'Error: ' . $e->getMessage()]);
+            exit;
+        }
+    } else {
+        echo json_encode(['status' => 'error', 'message' => 'One or more fields are empty']);
+        exit;
+    }
+}
+
+// Fetch product details if an ID is provided
+if (isset($_GET['id'])) {
+    $id = $_GET['id'];
+    $select = $conn->prepare("SELECT * FROM products WHERE status = 1 AND id = :id");
+    $select->execute([':id' => $id]);
     $product = $select->fetch(PDO::FETCH_OBJ);
 
-
-    // related products
-
-    $relatedProducts  = $conn -> query("SELECT * FROM products WHERE status= 1 AND category_id = '$product->category_id' AND id != '$id'");
-
-    $relatedProducts->execute();
-
+    // Fetch related products
+    $relatedProducts = $conn->prepare("SELECT * FROM products WHERE status = 1 AND category_id = :category_id AND id != :id");
+    $relatedProducts->execute([':category_id' => $product->category_id, ':id' => $id]);
     $allRelatedProducts = $relatedProducts->fetchAll(PDO::FETCH_OBJ);
-    // var_dump($allRelatedProducts);
+}
 
-
-    // Cart Logic
-    if(isset($POST['submit'])){
-        $pro_id= $_POST['pro_id'];
-        $pro_title = $_POST['pro_title'];
-        $pro_image= $_POST['pro_image'];
-        $pro_price= $_POST['pro_price'];
-        $pro_qty= $_POST['pro_qty'];
-        $user_id= $_POST['user_id'];
-
-        $insert = $conn->prepare("INSERT INTO cart (pro_id, pro_title,pro_image,pro_price,pro_qty,user_id) 
-        VALUES (:pro_id, :pro_title, :pro_image, :pro_price, :pro_qty, :user_id) ");
-        $insert->execute([
-
-        ':pro_id' => $pro_id,
-        ':pro_title' => $pro_title,
-
-        ':pro_image' => $pro_image,
-
-        ':pro_price' => $pro_price,
-        ':pro_qty' => $pro_qty,
-        ':user_id' => $user_id,
-        ]);
-    }
-}else{
-
+// validating cart products
+if(isset($_SESSION['user_id'])){
+$validate = $conn->query("SELECT * FROM cart WHERE pro_id='$id' AND user_id = '$_SESSION[user_id]'");
+$validate ->execute();
 }
 
 
 ?>
+
+
+
 <div id="page-content" class="page-content">
         <div class="banner">
             <div class="jumbotron jumbotron-bg text-center rounded-0" style="background-image: url('<?php echo APPURL; ?>/assets/img/bg-header.jpg');">
@@ -78,10 +96,10 @@ if(isset($_GET['id'])){
                             <?php echo $product->Description ?>
                         </p>
                         <div class="row">
-                            <div class="col-sm-6">
+                            <div class="col-sm-6">  
                                 <p>
                                     <strong>Price</strong> (/Pack)<br>
-                                    <span class="price"> $ <?php echo $product->Price ?></span>
+                              hidden      <span class="price"> $ <?php echo $product->Price ?></span>
                                     <!-- <span class="old-price"> <?php echo $product->price ?></span> -->
                                 </p>
                             </div>
@@ -93,33 +111,35 @@ if(isset($_GET['id'])){
                         <form method="POST" id="form-data">
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="pro_title"  value="<?php echo $product->Title ?> "">
+                                <!-- <input class="form-control" type="text" name="pro_title"  value="<?php echo $product->Title ?> ""> -->
+                                <input class="form-control" type="hidden" name="pro_title" value="<?php echo $product->Title; ?>">
+
                             </div>
                         
                         </div>
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="pro_id"  value="<?php echo $product->id ?> "">
+                                <input class="form-control" type="hidden" name="pro_id"  value="<?php echo $product->id ?> "">
                             </div>
                         
                         </div>
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text"name="pro_image"  value="<?php echo $product->image ?> "">
+                                <input class="form-control" type="hidden"name="pro_image"  value="<?php echo $product->image ?> "">
                             </div>
                         
                         </div>
 
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="pro_price" value="<?php echo $product->Price ?> "">
+                                <input class="form-control" type="hidden" name="pro_price" value="<?php echo $product->Price ?> "">
                             </div>
                         
                         </div>
 
                         <div class="row">
                             <div class="col-sm-5">
-                                <input class="form-control" type="text" name="user_id" value="<?php echo $_SESSION['user_id'] ?>" >
+                                <input class="form-control" type="hidden" name="user_id" value="<?php echo $_SESSION['user_id'] ?>" >
                             </div>
                         
                         </div> 
@@ -131,16 +151,24 @@ if(isset($_GET['id'])){
                             </div>
                             <div class="col-sm-6"><span class="pt-1 d-inline-block">Pack  (1000 grams)</span></div>
                         </div>
-                        <button name="submit" type="submit" class="btn-insert mt-3 btn btn-primary btn-lg">
+
+
+                        <?php if($validate->rowCount() > 0 ): ?> 
+                            <button name="submit" type="submit" class="btn-insert  mt-3 btn btn-primary btn-lg" disabled >
+                            <i class="fa fa-shopping-basket "></i> Added to Cart
+                        </button>
+                        <?php else : ?>
+                            <button name="submit" type="submit" class="btn-insert  mt-3 btn btn-primary btn-lg">
                             <i class="fa fa-shopping-basket"></i> Add to Cart
                         </button>
+                        <?php endif; ?>
                         </form>
                     </div>
                 </div>
             </div>
         </div>
 
-        <section id="related-product">
+        <section id="related-product">   
             <div class="container">
                 <div class="row">
                     <div class="col-md-12">
@@ -189,61 +217,35 @@ if(isset($_GET['id'])){
         </section>
 
     </div>
-    
+
+
     <?php require '../includes/footer.php' ?>
 
 
-    <!-- <script>
-        $(document).ready(function(){
-            $(".form-control").keyup(function(){
-                var value = $(this).val();
-                value = value.replace(/^(0*)/,"");
-                $(this).val(1);
-            });
-            $(".btn-insert").on("click", function(e){
-                e.preventDefault();
+<script>
+   $(document).ready(function(){
+       $(".form-control").keyup(function(){
+           var value = $(this).val();
+           value = value.replace(/^(0*)/,"");
+           $(this).val(1);
+       });
+       $(".btn-insert").on("click", function(e){
+           e.preventDefault();
 
-                var  form_data =  $("#form-data").serialize()+ '&submit=submit';
+           var  form_data =  $("#form-data").serialize()+ '&submit=submit';
 
-                $.ajax({
-                    url: "detail-product.php?id<?php echo $id ?>",
-                    method:"POST",
-                    data: form_data,
-                    success: function(){
-                        alert('Product added succesfully');
-                    }
-                })
-            })
-
-        })
-    </script> -->
+           $.ajax({
+               url: "detail-product.php?id<?php echo $id ?>",
+               method:"POST",
+               data: form_data,
+               success: function(){
+                   alert('Product added succesfully');
+                   $(".btn-insert").html("<i class='fa fa-shopping-basket'></i> Added to cart").prop("disabled", true);
 
 
-    <script>
-$(document).ready(function(){
-    $(".form-control").keyup(function(){
-        var value = $(this).val();
-        value = value.replace(/^(0*)/, ""); // Remove leading zeros
-        $(this).val(value ? value : 1); // Set to 1 if empty
-    });
-    
-    $(".btn-insert").on("click", function(e){
-        e.preventDefault();
+               }
+           })
+       })
 
-        var form_data = $("#form-data").serialize() + '&submit=submit';
-
-        $.ajax({
-            url: "detail-product.php?id=<?php echo $id; ?>",
-            method: "POST",
-            data: form_data,
-            success: function(){
-                console.log('Product added successfully'); // Debug log
-                alert('Product added successfully');
-            },
-            error: function(jqXHR, textStatus, errorThrown) {
-                console.error('AJAX Error:', textStatus, errorThrown); // Debug error
-            }
-        });
-    });
-});
-</script>
+   })
+</script> 
