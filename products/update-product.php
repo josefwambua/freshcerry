fafa16c9854a4598ae570cd669c7eda88d30955f
unknown <josefwambua2@gmail.com>
2024-11-15 13:03:12 +0300

    working-cart

--- products/update-product.php
+++ products/update-product.php
@@ -2,48 +2,33 @@
 <?php require '../config/config.php' ?>
  
 <?php
-
-// on clicking submit button
-
-if(isset($_POST['update'])){
-
+// Check if the request is POST and has required data
+if(isset($_POST['update'])) {
     $id = $_POST['id'];
     $pro_qty = $_POST['pro_qty'];
-    $pro_subtotal = $_POST['pro_subtotal'];
+    $subtotal = $_POST['subtotal'];
 
-    $update = $conn->prepare("UPDATE cart SET pro_qty = '$pro_qty', pro_subtotal = 'pro_subtotal' WHERE id='$id'");
-    $update->execute();
-
-
+    try {
+        // Use prepared statement to prevent SQL injection
+        $update = $conn->prepare("UPDATE cart SET pro_qty = :pro_qty, pro_subtotal = :subtotal WHERE id = :id");
+        
+        // Bind parameters
+        $update->bindParam(':pro_qty', $pro_qty, PDO::PARAM_INT);
+        $update->bindParam(':subtotal', $subtotal, PDO::PARAM_STR);
+        $update->bindParam(':id', $id, PDO::PARAM_INT);
+        
+        // Execute the query
+        $update->execute();
+        
+        // Send success response
+        echo "success";
+        
+    } catch(PDOException $e) {
+        // Handle errors
+        echo "Error: " . $e->getMessage();
+    }
+} else {
+    // If not a valid request
+    echo "Invalid request";
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 ?>
-
-
-
-
-
-
-
-<?php require "../includes/footer.php" ?>
